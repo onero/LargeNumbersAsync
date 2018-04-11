cf7969f09dcf68f3160ed167f20e1a3777c68a34
Adam Hansen <adamlars90@gmail.com>
2018-04-11 13:46:46 +0200

    Added error handling
    Handling for not entered number

--- app/src/main/java/dk/adamino/largenumbersasync/MainActivity.java
+++ app/src/main/java/dk/adamino/largenumbersasync/MainActivity.java
@@ -1,12 +1,13 @@
 package dk.adamino.largenumbersasync;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import dk.adamino.largenumbersasync.BLL.ILargeNumberCalculator;
 import dk.adamino.largenumbersasync.BLL.LargeNumberCalculator;
@@ -25,30 +26,34 @@ public class MainActivity extends AppCompatActivity implements IAsyncCalculation
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupViews();
+
+        mLargeNumberCalculator = new LargeNumberCalculator();
+    }
+
+    private void setupViews() {
         mNumber1 = findViewById(R.id.txtNumber1);
         mNumber2 = findViewById(R.id.txtNumber2);
         mResult = findViewById(R.id.txtResult);
         mProgressBar = findViewById(R.id.progressBar);
         mProgressBar.setVisibility(View.INVISIBLE);
-
-        mLargeNumberCalculator = new LargeNumberCalculator();
     }
 
     public void onCalculate(View view) {
-        long input1 = 0, input2 = 0;
-        if (hasInput(mNumber1)) {
+        long input1, input2;
+        if (hasInput(mNumber1) && hasInput(mNumber2)) {
             input1 = getLong(mNumber1);
-        }
-        if (hasInput(mNumber2)) {
             input2 = getLong(mNumber2);
-        }
 
-        try {
-            mLargeNumberCalculator.addAsync(this, input1, input2);
-            mProgressBar.setVisibility(View.VISIBLE);
-            mProgressBar.animate();
-        } catch (Exception oe) {
-            Log.e("GUI", oe.getMessage());
+            try {
+                mLargeNumberCalculator.addAsync(this, input1, input2);
+                mProgressBar.setVisibility(View.VISIBLE);
+                mProgressBar.animate();
+            } catch (Exception oe) {
+                Log.e("GUI", oe.getMessage());
+            }
+        } else {
+            Toast.makeText(this, R.string.error_enter_numbers, Toast.LENGTH_SHORT).show();
         }
     }
 
